257366a7dfa16acae8e0af46238a825922f14e40
GarubaHappiness <100312075+GarubaHappiness@users.noreply.github.com>
2023-02-19 17:29:40 +0000

    Add files via upload

---
+++ operatorscontrol-flow/index.js
@@ -0,0 +1,9 @@
+const fname = "Garuba";
+const lname = "Happiness";
+const stuternTrack = "Web Development";
+const favColor = "black";
+const independence = "1st of October, 1960"
+
+const sentence  = "My names is " + fname + " " + lname + ", a student currently studing " + stuternTrack + ", my favourite color is " + favColor + ", and my country gained independence on the " + independence;
+
+console.log(sentence)
